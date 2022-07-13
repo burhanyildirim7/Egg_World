52a5a2b7367ba73bb340ae1dbdb237e9c657e9c8
atakanhbk <84603321+atakanhbk@users.noreply.github.com>
2022-07-13 23:40:45 +0300

    UI düzeltildi

--- Assets/Scripts/CustomerSpawn.cs
+++ Assets/Scripts/CustomerSpawn.cs
@@ -39,7 +39,7 @@ public class CustomerSpawn : MonoBehaviour
        
         delayTime += Time.deltaTime;
 
-        if (delayTime >= 30 )
+        if (delayTime >= 100 )
         {
             //randomPlaceToSpawn = new Vector3(Random.Range(-5, 15), 1, Random.Range(17, 22));
             Instantiate(customer, transform.position, Quaternion.identity);
